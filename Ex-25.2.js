595bfb2fb3f10e6b78e98b8f6d2fbb2585e9256f
TofikSaliba <tofik.saliba@gmail.com>
2022-05-08 13:54:25 +0300

    added ex 25.2

---
+++ Ex-25.2.js
@@ -0,0 +1,12 @@
+const something = {
+  name: "Tofik",
+  printName() {
+    console.log(`I am ${this.name}`);
+  },
+  printNameDelay() {
+    setTimeout(this.printName.bind(this),1000)
+  }
+}
+
+something.printName();
+something.printNameDelay();
